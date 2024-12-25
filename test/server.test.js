1e297de10daa64ca9ece2ca9ef1dd9b0daea2fcd
ayushkanani711 <ayushkanani2003@gmail.com>
2024-12-25 19:39:22 +0530

    Failing test case for post request of add a new book

--- test/server.test.js
+++ test/server.test.js
@@ -8,15 +8,36 @@ describe("Server Running Check", () => {
     expect(response.statusCode).toBe(200);
     expect(response.text).toBe("Server is running");
   });
+
   //
   test("should respond to the GET request at the /api url", async () => {
     const response = await request(app).get("/api");
     expect(response.statusCode).toBe(200);
     expect(response.text).toBe("API is running");
   });
+
   //
   test("should respond 404 for a non-existent route", async () => {
     const response = await request(app).post("/api/non-existent-route");
     expect(response.statusCode).toBe(404);
   });
+
+  //
+  test("Should handle POST request", async () => {
+    const book = {
+      ISBN: "123456789123",
+      title: "The Alchemist",
+      author: "Paulo Coelho",
+      yearOfPublish: 1988,
+      available: true,
+      availableCopies: 10,
+    };
+    const response = await request(app).post("/api/books").send(book);
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toHaveProperty("message");
+    expect(response.body.success).toBe(true);
+    expect(response.body.message).toBe("Book added successfully");
+  });
+
+  //
 });
